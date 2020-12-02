5cd50bfeb545f81ef6b2ed60e43b48f944c11084
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-12-02 09:09:46 +0100

    feat: ✨ day template

--- src/day.template.ts
+++ src/day.template.ts
@@ -1,13 +1,15 @@
 import { Input, Solution } from "./lib"
 
 export const part1: Solution = (input: Input) => {
-  return input.data
+  input.data
+  return
 }
 part1.examples = []
 part1.answer = 0
 
 export const part2: Solution = (input: Input) => {
-  return input.data
+  input.data
+  return
 }
 part2.examples = []
 part2.answer = 0
